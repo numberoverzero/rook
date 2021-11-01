cf79bba68fed6b347dad45b4751beca395fb2f81
Joe Cross <joe.mcross@gmail.com>
2021-11-01 00:18:14 -0700

    first pass at config definition

---
+++ src/config.rs
@@ -0,0 +1,30 @@
+use toml;
+use std::collections::HashMap;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+pub struct RookConfig {
+    port: u16,
+    hooks: HashMap<String, Hook>
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type")]
+enum Hook {
+    #[serde(rename="github")]
+    GithubHook {repo: String, secret_path: String, script_path: String},
+}
+
+const SAMPLE_CONFIG: &str = r#"
+    port = 9000
+
+    [hooks.myhook]
+    type = "github"
+    repo = "numberoverzero/webhook-test"
+    secret_path = "/home/crossj/webhook-test-secret.txt"
+    script_path = "/home/crossj/webhook-test-script.sh"
+"#;
+
+pub fn parse_sample() -> RookConfig {
+    return toml::from_str(SAMPLE_CONFIG).unwrap();
+}

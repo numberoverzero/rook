cc80efad27d5ac9853183ce14224674ecfb251f5
Joe Cross <joe.mcross@gmail.com>
2021-11-08 16:24:12 -0800

    clean up example scripts

--- examples/github_hook.sh
+++ examples/github_hook.sh
@@ -3,4 +3,4 @@ echo "github: $(date +%s)"    >> output.log
 echo "  repo: $GITHUB_REPO"   >> output.log
 echo "commit: $GITHUB_COMMIT" >> output.log
 echo "   ref: $GITHUB_REF"    >> output.log
-echo "========================================"
+echo "========================================" >> output.log
